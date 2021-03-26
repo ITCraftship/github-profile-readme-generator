e9b53ae411296c56a56df5efaf567e92e3a17c94
Maksymilian Majer <1215868+maksymilian-majer@users.noreply.github.com>
2021-03-26 07:41:52 +0100

    feat(chat): chatwoot chat integration

--- src/html.js
+++ src/html.js
@@ -33,6 +33,24 @@ export default function HTML(props) {
         />
         {props.postBodyComponents}
       </body>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+    (function(d,t) {
+      var BASE_URL="https://chatwoot-itcraftship.herokuapp.com";
+      var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+      g.src=BASE_URL+"/packs/js/sdk.js";
+      s.parentNode.insertBefore(g,s);
+      g.onload=function(){
+        window.chatwootSDK.run({
+          websiteToken: 'iVU8TMoyHggj1v68rLxZEVcs',
+          baseUrl: BASE_URL
+        })
+      }
+    })(document,"script");
+              `,
+        }}
+      />
     </html>
   )
 }
